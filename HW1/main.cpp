2eff6dd791ff77f02cd9f165ef390b6d91630e9d
Arenooo <72516444+Arenooo@users.noreply.github.com>
2020-10-08 11:57:28 +0400

    Update main.cpp

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <vector>
 #include "complex.h"
-//#include "complex.cpp"
 
 void bubble_sort(std::vector<complex>& vec)
 {
-	for (int i = 0; i < vec.size() - 1; ++i)
-	{
-		for (int j = 0; j < vec.size() - i - 1; ++j)
-		{
-			if (vec[j].abs() > vec[j + 1].abs())
-				std::swap(vec[j], vec[j + 1]);
-		}
-	}
+        for (int i = 0; i < vec.size() - 1; ++i)
+        {
+                for (int j = 0; j < vec.size() - i - 1; ++j)
+                {
+                        if (vec[j].abs() > vec[j + 1].abs())
+                                std::swap(vec[j], vec[j + 1]);
+                }
+        }
 }
 
 int main()
 {
-	int n;
+        int n;
 
-	std::cin >> n;
+        std::cin >> n;
 
-	std::vector<complex> vec(n);
+        std::vector<complex> vec(n);
 
-	for (int i = 0; i < n; ++i)
-		std::cin >> vec[i].a >> vec[i].b;
+        for (int i = 0; i < n; ++i)
+                std::cin >> vec[i].a >> vec[i].b;
 
-	bubble_sort(vec);
+        bubble_sort(vec);
 
-	for (int i = 0; i < n; ++i)
-		std::cout << vec[i].a << ' ' << vec[i].b << '\n';
+        for (int i = 0; i < n; ++i)
+                std::cout << vec[i].a << " + " << vec[i].b << "i\n";
 
 
-	return 0;
+        return 0;
 }
